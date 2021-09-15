7390065d7519fda1fa6753a674d7187077448a7d
Ujjawal Pathak <ujjwalabhivns@gmail.com>
2021-09-15 15:22:21 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sed -i "s~#{image}~$ARTIFACT_IMAGE~g" ./gradle-helm/values.yaml
+sed -i "s~@{image}~$ARTIFACT_IMAGE~g" ./gradle-helm/values.yaml
 helm install gradle-demo ./gradle-helm/.
